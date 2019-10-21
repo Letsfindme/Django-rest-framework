3649f115f55479ce389c512dae362b3640866973
Fadi NOUFAL <noufal4me@gmail.com>
2019-10-21 22:07:10 +0200

    Create super user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
-from djando.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@londonappdev.com'
+        email = 'test@noufal4me.com'
         password = 'Password123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -15,3 +15,24 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@NOUFAL4ME.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@noufal4me.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
